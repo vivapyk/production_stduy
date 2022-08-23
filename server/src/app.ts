ba9a9715a358adb39d8e4ccbed50e56847f941a6
Young_home <vivapyk@gmail.com>
2022-08-23 23:30:24 +0900

    add retrospect endpoint

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
+import bodyParser from 'body-parser'
 
 dotenv.config();
 
@@ -12,9 +13,8 @@ const options: cors.CorsOptions = {
     origin: allowedOrigins
 }
 
-
 app.use(cors(options))
-
+app.use(bodyParser.json());
 
 const port = process.env.PORT
 
@@ -23,7 +23,13 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 app.get('/intro', (req: Request, res: Response) => {
+    console.log('hi')
     res.send({ intro: 'introduction' })
+});
+
+app.post('/retrospect', (req: Request, res: Response) => {
+    console.log(req.body)
+    res.send({ retrospect: 'retrospect' })
 });
 
 app.listen(port, () => {
